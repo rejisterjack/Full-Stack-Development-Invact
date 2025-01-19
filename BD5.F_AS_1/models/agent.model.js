26f0f2e0d2be96fa05428ba31d549aefc7fa06cd
Rupam Das <rejisterjack@gmail.com>
2025-01-19 17:57:31 +0530

    define models

---
+++ BD5.F_AS_1/models/agent.model.js
@@ -0,0 +1,19 @@
+const { sequelize, DataTypes } = require("../lib")
+
+const agent = sequelize.define("agent", {
+  agentId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
+
+module.exports = { agent }

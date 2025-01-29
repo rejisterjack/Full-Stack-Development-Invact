acc49bf68679a0bb16f8119aaff7dedc2c3b786a
Rupam Das <rejisterjack@gmail.com>
2025-01-29 19:21:03 +0530

    movie and index

---
+++ BD6.1_HW_1/movie.js
@@ -0,0 +1,22 @@
+const movies = [
+  { id: 1, title: "The Shawshank Redemption", director: "Frank Darabont" },
+  { id: 2, title: "The Godfather", director: "Francis Ford Coppola" },
+  { id: 3, title: "The Dark Knight", director: "Christopher Nolan" },
+  { id: 4, title: "Pulp Fiction", director: "Quentin Tarantino" },
+]
+
+export const getMovies = () => movies
+
+export const getMovieById = (id) => {
+  return getMovies().find((item) => item.id === id)
+}
+
+export const addMovie = (movie) => {
+  const newMovie = {
+    id: movie.id,
+    title: movie.title,
+    director: movie.director,
+  }
+  movies.push(newMovie)
+  return newMovie
+}

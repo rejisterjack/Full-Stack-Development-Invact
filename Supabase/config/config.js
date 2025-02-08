4ce3a1a4657ba48e6f02d7057bdd5c9f3835235b
Rupam Das <rejisterjack@gmail.com>
2025-02-08 12:37:47 +0530

    supabase

--- Supabase/config/config.js
+++ Supabase/config/config.js
@@ -1,30 +1,48 @@
 require("dotenv").config()
 
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     logging: false,
+//   },
+//   test: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     logging: false,
+//   },
+//   production: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     logging: false,
+//   },
+// }
+
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     logging: false,
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     logging: false,
   },

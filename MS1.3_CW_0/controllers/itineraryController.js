cf3ee9dea5e7b55f9592780445d380b7554b961b
Rupam Das <rejisterjack@gmail.com>
2025-02-14 08:29:22 +0530

    feat: rate limit implemented

--- MS1.3_CW_0/controllers/itineraryController.js
+++ MS1.3_CW_0/controllers/itineraryController.js
@@ -11,10 +11,23 @@ const axiosInstance = axios.create({
 
 const getFlights = async (req, res) => {
   try {
-    const response = await axiosInstance.get("/flights")
+    const response = await axiosInstance.get(
+      `/flights?test_error=${req.query.test_error}&rate_limit=${req.query.rate_limit}`
+    )
     res.send(response.data)
   } catch (error) {
     console.log(error.message)
+
+    if (error.response.status === 429) {
+      res.status(429).send("Rate limit exceeded")
+      return
+    } else if (
+      error.response.status === 500 &&
+      error.response.data.error === "Simulated error for testing purposes"
+    ) {
+      res.status(500).send("Simulated error for testing purposes")
+      return
+    }
     res.status(500).send("Error fetching flights")
   }
 }
@@ -25,6 +38,18 @@ const getHotels = async (req, res) => {
     res.send(response.data)
   } catch (error) {
     console.log(error.message)
+
+    if (error.response.status === 429) {
+      res.status(429).send("Rate limit exceeded")
+      return
+    } else if (
+      error.response.status === 500 &&
+      error.response.data.error === "Simulated error for testing purposes"
+    ) {
+      res.status(500).send("Simulated error for testing purposes")
+      return
+    }
+
     res.status(500).send("Error fetching hotels")
   }
 }
@@ -35,6 +60,18 @@ const getSites = async (req, res) => {
     res.send(response.data)
   } catch (error) {
     console.log(error.message)
+
+    if (error.response.status === 429) {
+      res.status(429).send("Rate limit exceeded")
+      return
+    } else if (
+      error.response.status === 500 &&
+      error.response.data.error === "Simulated error for testing purposes"
+    ) {
+      res.status(500).send("Simulated error for testing purposes")
+      return
+    }
+
     res.status(500).send("Error fetching sites")
   }
 }

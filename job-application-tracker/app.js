d0783c4134d8a5b73323e6c9ac52e35826d0f13a
Rupam Das <rejisterjack@gmail.com>
2025-03-18 23:46:02 +0530

    feat: get applications feat: application by id

--- job-application-tracker/app.js
+++ job-application-tracker/app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { Op } = require("sequelize")
 const Application = require("./models/Application.model")
 
 const app = express()
@@ -27,6 +28,48 @@ app.post("/applications", async (req, res) => {
       appliedAt: appliedAt || new Date(),
     })
     res.status(201).json(application)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send("Server Error")
+  }
+})
+
+app.get("/applications", async (req, res) => {
+  try {
+    const { company, status, from, to } = req.query
+    const filter = {}
+
+    if (company) {
+      filter.company = company
+    }
+    if (status) {
+      filter.status = status
+    }
+    if (from || to) {
+      filter.appliedAt = {}
+      if (from) {
+        filter.appliedAt[Op.gte] = new Date(from)
+      }
+      if (to) {
+        filter.appliedAt[Op.lte] = new Date(to)
+      }
+    }
+
+    const applications = await Application.findAll({ where: filter })
+    res.status(200).json(applications)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send("Server Error")
+  }
+})
+
+app.get("/applications/:id", async (req, res) => {
+  try {
+    const application = await Application.findByPk(req.params.id)
+    if (!application) {
+      return res.status(404).send("Application not found")
+    }
+    res.status(200).json(application)
   } catch (error) {
     console.error(error.message)
     res.status(500).send("Server Error")
